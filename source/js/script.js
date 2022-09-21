61ef4578f55141518df2629d82ebcc5fdaecac9d
VovaLovalova <rudchenko.vova@mail.ru>
2022-09-21 21:53:03 +0300

    Project v2

--- source/js/script.js
+++ source/js/script.js
@@ -2,14 +2,14 @@ let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
-navMain.classList.add('main-nav--closed');
+navMain.classList.add('header-box--closed');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('header-box--closed')) {
+    navMain.classList.remove('header-box--closed');
+    navMain.classList.add('header-box--opened');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('header-box--closed');
+    navMain.classList.remove('header-box--opened');
   }
 });
